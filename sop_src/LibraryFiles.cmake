24cd5cafc602c6a333fa6f47c3c88705b52ea716
Chris Fogelklou <chris.fogelklou@polestar.com>
2020-01-15 07:02:28 +0100

    Rename some defines.

--- sop_src/LibraryFiles.cmake
+++ sop_src/LibraryFiles.cmake
@@ -9,7 +9,7 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
   add_definitions("/wd4146 /wd4244 /wd4996 -D_SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING")
 endif ()
 
-add_definitions(-DMBEDTLS_CONFIG_FILE="src/mbedtls/myconfig.h")
+add_definitions(-DMBEDTLS_CONFIG_FILE="sop_src/mbedtls/myconfig.h")
 
 # USE recursive search to add all libsodium files.
 file(GLOB_RECURSE LIBSODIUM_SRC
@@ -31,8 +31,8 @@ file(GLOB MBEDTLS_SRC
   ${SOP_EXTERN_LIBS}/mbedtls/library/ed25519/*.c
   ${SOP_EXTERN_LIBS}/mbedtls/library/ed25519/*.h
   ${SOP_EXTERN_LIBS}/mbedtls/crypto/library/*.c
-  ${SOP_TOP_DIR}/src/mbedtls/*.cpp
-  ${SOP_TOP_DIR}/src/mbedtls/*.c
+  ${SOP_COMMON_SRC}/mbedtls/*.cpp
+  ${SOP_COMMON_SRC}/mbedtls/*.c
 )
 
 list(REMOVE_ITEM MBEDTLS_SRC ${SOP_EXTERN_LIBS}/mbedtls/crypto/library/version.c)
@@ -41,39 +41,39 @@ list(REMOVE_ITEM MBEDTLS_SRC ${SOP_EXTERN_LIBS}/mbedtls/library/version_features
 
 
 file(GLOB TASK_SCHED_SRC
-        ${SOP_TOP_DIR}/src/task_sched/*.cpp
-        ${SOP_TOP_DIR}/src/task_sched/*.c
-        ${SOP_TOP_DIR}/src/task_sched/*.h
-        ${SOP_TOP_DIR}/src/task_sched/*.hpp
+        ${SOP_COMMON_SRC}/task_sched/*.cpp
+        ${SOP_COMMON_SRC}/task_sched/*.c
+        ${SOP_COMMON_SRC}/task_sched/*.h
+        ${SOP_COMMON_SRC}/task_sched/*.hpp
 )
 
 file(GLOB OSAL_SRC
-        ${SOP_TOP_DIR}/src/osal/*.cpp
-        ${SOP_TOP_DIR}/src/osal/*.c
-        ${SOP_TOP_DIR}/src/osal/*.h
-        ${SOP_TOP_DIR}/src/osal/*.hpp
+        ${SOP_COMMON_SRC}/osal/*.cpp
+        ${SOP_COMMON_SRC}/osal/*.c
+        ${SOP_COMMON_SRC}/osal/*.h
+        ${SOP_COMMON_SRC}/osal/*.hpp
 )
 
 file(GLOB BUF_IO_SRC
-        ${SOP_TOP_DIR}/src/buf_io/*.cpp
-        ${SOP_TOP_DIR}/src/buf_io/*.c
-        ${SOP_TOP_DIR}/src/buf_io/*.h
-        ${SOP_TOP_DIR}/src/buf_io/*.hpp
+        ${SOP_COMMON_SRC}/buf_io/*.cpp
+        ${SOP_COMMON_SRC}/buf_io/*.c
+        ${SOP_COMMON_SRC}/buf_io/*.h
+        ${SOP_COMMON_SRC}/buf_io/*.hpp
 )
 
 file(GLOB UTILS_SRC
-        ${SOP_TOP_DIR}/src/utils/*.cpp
-        ${SOP_TOP_DIR}/src/utils/*.c
-        ${SOP_TOP_DIR}/src/utils/*.h
-        ${SOP_TOP_DIR}/src/utils/*.hpp
+        ${SOP_COMMON_SRC}/utils/*.cpp
+        ${SOP_COMMON_SRC}/utils/*.c
+        ${SOP_COMMON_SRC}/utils/*.h
+        ${SOP_COMMON_SRC}/utils/*.hpp
 )
 
 
 file(GLOB MINI_SOCKET_SRC
-        ${SOP_TOP_DIR}/src/mini_socket/*.c
-        ${SOP_TOP_DIR}/src/mini_socket/*.cpp
-        ${SOP_TOP_DIR}/src/mini_socket/*.h
-        ${SOP_TOP_DIR}/src/mini_socket/*.hpp
+        ${SOP_COMMON_SRC}/mini_socket/*.c
+        ${SOP_COMMON_SRC}/mini_socket/*.cpp
+        ${SOP_COMMON_SRC}/mini_socket/*.h
+        ${SOP_COMMON_SRC}/mini_socket/*.hpp
 )
 
 set(GTEST_SRC
