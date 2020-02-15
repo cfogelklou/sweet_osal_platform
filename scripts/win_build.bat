abd1b28d103bdf8a194fdf23b63080d2cc9429cc
Chris Fogelklou <chris.fogelklou@polestar.com>
2020-02-15 14:55:19 +0100

    try this.

--- scripts/win_build.bat
+++ scripts/win_build.bat
@@ -1,11 +1,13 @@
         setlocal
         call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
-        path "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin";%path%
+        path "C:\Program Files (x86)\MSBuild\16.0\Bin;C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin";%path%
         mkdir build
         cd build
-        cmake ../
+        cmake -G "Visual Studio 16 2019" ..
+        echo "Stuff that's here."
+        dir
         echo "Preparing to build."
-        call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe ./sweet_osal_platform.sln /property:Configuration=Debug -maxcpucount:4"
+        msbuild sweet_osal_platform.sln /property:Configuration=Debug -maxcpucount:4
         cd ../
 
         endlocal
