4aeada01e0d1fe3d87401a90e0e7b17a762fd755
Chris Fogelklou <chris.fogelklou@gmail.com>
2022-03-06 08:32:14 +0100

    Remove redundant test.

--- sop_src/utils/tests/test_sstring.cpp
+++ sop_src/utils/tests/test_sstring.cpp
@@ -1,30 +1,30 @@
-#ifdef RUN_GTEST
+
+#include <string.h>
+#include "gtest/gtest.h"
 #include "osal/osal.h"
-#include "tests/gtest_test_wrapper.hpp"
-#include "utils/convert_utils.h"
-#include "utils/helper_macros.h"
 #include "utils/platform_log.h"
 #include "utils/util_vsnprintf.h"
-
-#include "gtest/gtest.h"
+#include "utils/helper_macros.h"
+#include "utils/convert_utils.h"
+#include "utils/simple_string.hpp"
+#include "tests/gtest_test_wrapper.hpp"
 #include <string.h>
+
 LOG_MODNAME("test_sstring.cpp");
 
 class TestSstring : public GtestMempoolsWrapper {
 public:
-  TestSstring() {
-  }
-  ~TestSstring() {
-  }
+  TestSstring(){}
+  ~TestSstring() {}
 };
 
-TEST_F(TestSstring, test_vsnprintf) {
-  char dst[ 20 ];
+TEST_F(TestSstring, test_vsnprintf){
+  char dst[20];
   {
     const char goal[] = "hex = 0x12345678";
     int dstlen = util_snprintf(dst, ARRSZ(dst), "hex = 0x%x", 0x12345678);
-    ASSERT_TRUE(dstlen == strlen(goal));
-    ASSERT_TRUE(0 == memcmp(goal, dst, dstlen));
+    ASSERT_TRUE( dstlen == strlen(goal));
+    ASSERT_TRUE( 0 == memcmp(goal, dst, dstlen));
   }
   {
 #ifdef USE_UTIL_SPRINTF
@@ -33,20 +33,20 @@ TEST_F(TestSstring, test_vsnprintf) {
     const char goal[] = "hex = 0x678";
 #endif
     int dstlen = util_snprintf(dst, ARRSZ(dst), "hex = 0x%x", 0x00000678);
-    ASSERT_TRUE(dstlen == strlen(goal));
-    ASSERT_TRUE(0 == memcmp(goal, dst, dstlen));
+    ASSERT_TRUE( dstlen == strlen(goal));
+    ASSERT_TRUE( 0 == memcmp(goal, dst, dstlen));
   }
   {
     const char goal[] = "dec = 22";
     int dstlen = util_snprintf(dst, ARRSZ(dst), "dec = %d", 22);
-    ASSERT_TRUE(dstlen == strlen(goal));
-    ASSERT_TRUE(0 == memcmp(goal, dst, dstlen));
+    ASSERT_TRUE( dstlen == strlen(goal));
+    ASSERT_TRUE( 0 == memcmp(goal, dst, dstlen));
   }
   {
     const char goal[] = "dec = -22";
     int dstlen = util_snprintf(dst, ARRSZ(dst), "dec = %d", -22);
-    ASSERT_TRUE(dstlen == strlen(goal));
-    ASSERT_TRUE(0 == memcmp(goal, dst, dstlen));
+    ASSERT_TRUE( dstlen == strlen(goal));
+    ASSERT_TRUE( 0 == memcmp(goal, dst, dstlen));
   }
 }
 
@@ -75,6 +75,16 @@ TEST_F(TestSstring, cnv_atoi) {
 
   r = CNV_atoi("89102");
   EXPECT_EQ(r, 89102);
+
 }
 
-#endif
+TEST_F(TestSstring, cnv_uuidify) {
+  const char hexIn[] = "0123456789ABCDEF0123456789ABCDEF";  
+  const char out[] = "01234567-89AB-CDEF-0123-456789ABCDEF";
+  
+  sstring s;
+  CNV_Uuidify(hexIn, s);
+  ASSERT_TRUE(0 == strncmp(s.c_str(), out, strlen(out)));
+
+}
+
